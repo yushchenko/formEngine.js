af1e457e678472c0d03d6f419daf62aa792e7aaa
Valery Yushchenko <valery.yushchenko@gmail.com>
2010-12-28 21:41:32 +0200

    combining added to build

--- source/fe.js
+++ source/fe.js
@@ -1,10 +1,2 @@
-/*
- * FormEngine.js - MVC on steroids :)
- * http://github.com/yushchenko/formEngine.js
- *
- * Copyright 2010, Valery Yushchenko (http://www.yushchenko.name)
- * Dual licensed under the MIT or GPL Version 2 licenses.
- * 
- */
  
 var fe = {};
---
+++ source/intro.js
@@ -0,0 +1,11 @@
+/*
+ * FormEngine.js - MVC on steroids :)
+ * http://github.com/yushchenko/formEngine.js
+ *
+ * Copyright 2010, Valery Yushchenko (http://www.yushchenko.name)
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * 
+ */
+
+(function( global, undefined ) {
+
