21967b56bef5c65d938c55d9a0dc1cc0d8aa1182
Valery Yushchenko <valery.yushchenko@gmail.com>
2011-01-01 15:16:17 +0200

    engine - message transmission according rules

--- source/fe.engine.js
+++ source/fe.engine.js
@@ -1,13 +1,78 @@
 
 fe.engine = function engine(config) {
 
-    var that = {};
+    var that = {},
+        receivers = {},
+        rules = [];
 
     function addReceiver(id, receiver) {
+
+        if (id in receivers) {
+            throw new Error('engine.addReceiver: recevier with given ID has been already added.');
+        }
+        if (typeof id !== 'string') {
+            throw new Error('engine.addReceiver: id must be string');
+        }
+        if (!receiver || typeof receiver.receiveMessage !== 'function') {
+            throw new Error('engine.addReceiver: receiver should have method "receiveMessage"');
+        }
+
+        receivers[id] = receiver;
     }
 
     function addRule(rule) {
-        
+
+        if (typeof rule.receiverId !== 'string') {
+            throw new Error('engine.addRule: rule must have receiverId property, type string');
+        }
+
+        var r = {},
+            properties = ['senderId', 'path', 'signal'],
+            checkers = ['checkSender', 'checkPath', 'checkSignal'],
+            checkStartWith = [false, true, false],
+            i, len = properties.length;
+
+        r.receiverId = rule.receiverId;
+
+        for (i = 0; i < len; i += 1) { (function(property, checker, checkStartWith) {
+
+            if (property) {
+                r[checker] = function (arg) {
+                    var ii, ll;
+                    if (typeof property === 'string') {
+                        return checkStartWith ? arg.indexOf(property) === 0 : arg === property;
+                    }
+                    if (typeof property === 'object' && property.length) {
+                        for (ii = 0, ll = property.length; ii < ll; ii += 1) {
+                            if (checkStartWith ? arg.indexOf(property[ii]) === 0 : arg === property[ii]) {
+                                return true;
+                            }
+                        }
+                        return false;
+                    }
+                    return true;
+                };
+            }
+
+        })(rule[properties[i]], checkers[i], checkStartWith[i]);}
+
+        rules.push(r);
+    }
+
+    function addRules(/*rules in array or arguments*/) {
+
+        var i, l = arguments.length,
+            ii, ll;
+
+        for(i = 0; i < l; i += 1) {
+            if (arguments[i].length) {
+                for (ii = 0, ll = arguments[i].length; ii < ll; ii += 1) {
+                    addRule(arguments[i][ii]);
+                }
+            } else {
+                addRule(arguments[i]);
+            }
+        }
     }
 
     function addTrigger(trigger) {
@@ -15,11 +80,32 @@ fe.engine = function engine(config) {
     }
 
     function sendMessage(message) {
-        
+
+        var i, len = rules.length, rule;
+
+        function send() {
+            receivers[rule.receiverId].receiveMessage(message);
+        }
+
+        for (i = 0; i < len; i += 1) {
+            rule = rules[i];
+
+            if (message.senderId === rule.receiverId) {
+                continue;
+            }
+
+            if ((!rule.checkSender || rule.checkSender(message.senderId)) &&
+                (!rule.checkPath || rule.checkPath(message.path)) &&
+                (!rule.checkSignal || rule.checkSignal(message.signal))) {
+
+                send();
+            }
+        }
     }
 
     that.addReceiver = addReceiver;
     that.addRule = addRule;
+    that.addRules = addRules;
     that.addTrigger = addTrigger;
     that.sendMessage = sendMessage;
     
--- tests/source/fe.engine.tests.js
+++ tests/source/fe.engine.tests.js
@@ -14,4 +14,95 @@ describe('fe.engine', function() {
         expect(typeof e.addTrigger).toEqual('function');
         expect(typeof e.sendMessage).toEqual('function');
     });
+
+    it('should send simple message (event subscription)', function() {
+
+        var e = fe.engine(),
+            receiver = { receiveMessage: jasmine.createSpy('receiveMessage') },
+            rule = { receiverId: 'r1', senderId: 's1', signal: 'test' },
+            message =  { senderId: 's1', signal: 'test', data: 'test data' };
+
+        e.addReceiver('r1', receiver);
+        e.addRule(rule);
+
+        e.sendMessage(message);
+
+        expect(receiver.receiveMessage).toHaveBeenCalled();
+    });
+
+    it('should send messages from control to model', function() {
+
+        var e = fe.engine(),
+            receiver = { receiveMessage: jasmine.createSpy('receiveMessage') },
+            rule = { receiverId: 'r1', path: 'a', signal: 'value' },
+            message1 =  { senderId: 's1', path: 'a.b.c', signal: 'value', data: 'test data c' },
+            message2 =  { senderId: 's1', path: 'a.b.d', signal: 'value', data: 'test data d' };
+
+        e.addReceiver('r1', receiver);
+        e.addRule(rule);
+
+        e.sendMessage(message1);
+        expect(receiver.receiveMessage).toHaveBeenCalledWith(message1);
+
+        e.sendMessage(message2);
+        expect(receiver.receiveMessage).toHaveBeenCalledWith(message2);
+    });
+
+    it('should send messages from model to control', function() {
+
+        var e = fe.engine(),
+            receiver = { receiveMessage: jasmine.createSpy('receiveMessage')},
+            rule = { receiverId: 'r1', path: 'a.b.c', signal: 'value' },
+            msg1 = { senderId: 'm1', path: 'a.b.c', signal: 'value', data: 'test a.b.c'},
+            msg2 = { senderId: 'm1', path: 'a.b.d', signal: 'value', data: 'test a.b.d'};
+
+        e.addReceiver('r1', receiver);
+        e.addRule(rule);
+
+        e.sendMessage(msg1);
+        e.sendMessage(msg2);
+
+        expect(receiver.receiveMessage).toHaveBeenCalledWith(msg1);
+    });
+
+    it('should send message to trigger', function() {
+
+        var e = fe.engine(),
+            receiver = { receiveMessage: jasmine.createSpy('receiveMessage')},
+            rule = { receiverId: 'r1', path: ['p1', 'p2'], signal: 'value' },
+            msg1 = { senderId: 'm1', path: 'p1', signal: 'value', data: 'test p1'},
+            msg2 = { senderId: 'm2', path: 'p2', signal: 'value', data: 'test p2'},
+            msg3 = { senderId: 'm3', path: 'p_other', signal: 'value', data: 'test p other'};
+
+        e.addReceiver('r1', receiver);
+        e.addRule(rule);
+
+        e.sendMessage(msg1);
+        expect(receiver.receiveMessage).toHaveBeenCalledWith(msg1);
+
+        e.sendMessage(msg2);
+        expect(receiver.receiveMessage).toHaveBeenCalledWith(msg2);
+
+        e.sendMessage(msg3);
+        expect(receiver.receiveMessage).not.toHaveBeenCalledWith(msg3);
+    });
+
+    it('should send message from trigger', function() {
+
+        var e = fe.engine(),
+            c1 = { receiveMessage: jasmine.createSpy('c1.receiveMessage')},
+            c2 = { receiveMessage: jasmine.createSpy('c2.receiveMessage')},
+            rule1 = { receiverId: 'c1', senderId: 't', signal: 'hidden' },
+            rule2 = { receiverId: 'c2', senderId: 't', signal: 'hidden' },
+            msg = { senderId: 't', signal: 'hidden', data: true };
+        
+        e.addReceiver('c1', c1);
+        e.addReceiver('c2', c2);
+        e.addRules([rule1, rule2]);
+
+        e.sendMessage(msg);
+        expect(c1.receiveMessage).toHaveBeenCalledWith(msg);
+        expect(c2.receiveMessage).toHaveBeenCalledWith(msg);
+    });
+
 });
